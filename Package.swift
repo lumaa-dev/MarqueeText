c6d7b06aa8df4c13a4cb3255991e21541564cc07
Loopy <loopy@heyratel.com>
2025-10-10 13:40:01 +0900

    change supoorted platform ios 14

--- Package.swift
+++ Package.swift
@@ -6,17 +6,17 @@ import PackageDescription
 let package = Package(
     name: "MarqueeText",
     platforms: [
-        SupportedPlatform.iOS(.v13),
+        SupportedPlatform.iOS(.v14),
         SupportedPlatform.macOS(.v10_15),
         SupportedPlatform.watchOS(.v7),
-        SupportedPlatform.tvOS(.v13)
+        SupportedPlatform.tvOS(.v13),
 
     ],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "MarqueeText",
-            targets: ["MarqueeText"]),
+            targets: ["MarqueeText"])
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
